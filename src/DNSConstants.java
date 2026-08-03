9522607b337468ad71a41d31377fb67db66b86ff
baljabran <bayan.al-jubran@city.ac.uk>
2026-08-03 23:24:47 +0300

    Add DNSConstants class with DNS record types and codes

--- src/DNSConstants.java
+++ src/DNSConstants.java
@@ -1,1 +1,21 @@
+public class DNSConstants {
 
+    // DNS record types
+    public static final short TYPE_A = 1;
+    public static final short TYPE_NS = 2;
+    public static final short TYPE_CNAME = 5;
+    public static final short TYPE_MX = 15;
+    public static final short TYPE_TXT = 16;
+
+    // DNS class
+    public static final short CLASS_IN = 1;
+
+    // Standard DNS port
+    public static final int DNS_PORT = 53;
+
+    // Response codes
+    public static final int RCODE_NOERROR = 0;
+    public static final int RCODE_FORMERR = 1;
+    public static final int RCODE_SERVFAIL = 2;
+    public static final int RCODE_NXDOMAIN = 3;
+}
