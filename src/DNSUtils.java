2b7be56e4405a520c1ff53d2ce0dcc8ce9bf45b8
baljabran <bayan.al-jubran@city.ac.uk>
2026-08-03 23:27:20 +0300

    Add methods to write and read short values

--- src/DNSUtils.java
+++ src/DNSUtils.java
@@ -3,4 +3,13 @@ import java.nio.ByteBuffer;
 
 public class DNSUtils {
 
+    public static void writeShort(ByteArrayOutputStream out, int value) {
+        out.write((value >> 8) & 0xFF);
+        out.write(value & 0xFF);
+    }
+
+    public static int readShort(ByteBuffer buffer) {
+        return buffer.getShort() & 0xFFFF;
+    }
+
 }
